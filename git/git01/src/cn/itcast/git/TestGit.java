3e66870c229b16992c36597a21f3bc73808910c3
al <al@126.com>
2019-09-15 20:30:12 +0800

    修改git01工程、添加到本地仓库、再推送到远程仓库

--- git/git01/src/cn/itcast/git/TestGit.java
+++ git/git01/src/cn/itcast/git/TestGit.java
@@ -8,4 +8,11 @@ public class TestGit {
         System.out.println("Intellij IDEA 中使用git");
         System.out.println("Intellij IDEA 中使用git");
     }
+
+    public void addMethod(){
+
+        System.out.println("修改：添加一个方法");
+        System.out.println("然后提交到远程仓库");
+    }
+
 }
