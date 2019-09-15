58f20cd12234c2ec673526a0af42d3f067b78257
al <al@126.com>
2019-09-15 18:10:12 +0800

    添加git01工程到git（将工程添加到本地仓库） 然后推送到远程仓库github

---
+++ git/git01/src/cn/itcast/git/TestGit.java
@@ -0,0 +1,11 @@
+package cn.itcast.git;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("Intellij IDEA 中使用git");
+        System.out.println("Intellij IDEA 中使用git");
+        System.out.println("Intellij IDEA 中使用git");
+        System.out.println("Intellij IDEA 中使用git");
+    }
+}
